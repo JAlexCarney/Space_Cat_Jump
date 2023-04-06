92b8f472ea084e8840987b7c7fea7de91deb150a
JAlexCarney <JAlexCarney@gmail.com>
2023-04-05 22:00:20 -0500

    Cat speeeeeen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float currentSpeed = 0f;
+    [SerializeField] private float acceleration = 0.5f;
+    [SerializeField] private float deceleration = 1f;
+    [SerializeField] private float skidDeceleration = 2f;
+    [SerializeField] private float maxMoveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
     private enum MovementState { idle, running, jumping, falling }
@@ -29,18 +33,46 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        var deltaX = dirX * acceleration;
+        if (IsGrounded())
+        {
+            if (deltaX == 0f && currentSpeed != 0)
+            {
+                // Decelerate
+                currentSpeed += -1f * Mathf.Sign(currentSpeed) * deceleration;
+            }
+            else if (deltaX != 0f && currentSpeed != 0 && Mathf.Sign(currentSpeed) != Mathf.Sign(deltaX))
+            {
+                // Skid
+                currentSpeed += deltaX * skidDeceleration;
+            }
+            else
+            {
+                // Accelerating
+                currentSpeed += deltaX;
+            }
+        }
+        else 
+        {
+            currentSpeed += deltaX;
+        }
+        
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        currentSpeed = Mathf.Clamp(currentSpeed, -maxMoveSpeed, maxMoveSpeed);
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+        UpdateAnimationState();
+    }
+
+    private void Update()
+    {
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
         {
             jumpSoundEffect.Play();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(currentSpeed, jumpForce);
         }
-
-        UpdateAnimationState();
     }
 
     private void UpdateAnimationState()
